113738475aaefe5a1fb3327c266c4892391cdae2
wutong <wutongtoby@gmail.com>
2020-03-11 17:28:56 +0800

    1

---
+++ 1_1_Blinky_LED/main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+DigitalOut redLED(LED1);
+DigitalOut greenLED(LED2);
+
+int main(void)
+{
+    while (1) {
+        redLED = 1;
+        greenLED = 0;
+        wait(0.2);
+        redLED = 0;
+        greenLED = 1;
+        wait(0.2);
+    }
+}
